548f8323176857087f1688bf768ef7a24e05aad5
Solomon <solomonkpodo@outlook.com>
2020-01-28 14:58:40 -0600

    s 20200128a test

--- KenkoWebApp/Controllers/HomeController.cs
+++ KenkoWebApp/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace KenkoWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;v
 
         public HomeController(ILogger<HomeController> logger)
         {
